7748fb50bdf7946971a25ae034c999834eb883ad
Vero1806 <146030980+Vero1806@users.noreply.github.com>
2023-12-19 20:25:29 +0100

    Diapositivas de mañana

--- app/src/main/java/com/example/inventory/data/Database.kt
+++ app/src/main/java/com/example/inventory/data/Database.kt
@@ -1,5 +1,8 @@
 package com.example.inventory.data
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
 import kotlin.jvm.Volatile
 
@@ -11,6 +14,17 @@ abstract class ItemRoomDatabase : RoomDatabase() {
         companion object {
             @Volatile
             private var INTANCE: ItemRoomDatabase? = null
+            fun getDatabase (context: Context): ItemRoomDatabase{
+                return INTANCE ?: synchronized(this) {
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ItemRoomDatabase::class.java,
+                        "item_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INTANCE = instance
+                    return instance
+                }
+            }
         }
-
 }
